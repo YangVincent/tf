fe4d9352bdb84fdfdad06a4b2f3aa1bc5104d68d
Vincent Yang <vincenty.376@gmail.com>
2018-08-15 22:00:08 -0700

    KNN

---
+++ nn/nn.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class NearestNeighbor:
+    def __init__(self):
+        pass
+
+    def train(self, X, y):
+        """ X is N x D where each row is an example. Y is 1-dimension of size N"""
+        # nearest neighbor classifier remembers all training data
+        self.Xtr = X
+        self.ytr = y
+
+    def predict(self, X):
+        """ X is N x D where each row is an example we wish to predict label for"""
+        num_test = X.shape[0]
+        # make sure output type matches input type
+        # Ypred = np.zeros(num_test, dtype = self.ytr.dtype)
+        
+        # loop over all test rows
+        for i in xrange(num_test):
+            # find nearest training image to the i'th test image
+            # using L1 distance (sum of absolute value distance)
+            distances = np.sum(np.abs(self.Xtr - X[i,:]), axis = 1)
+            min_index = np.argmin(distances) # get index with smallest distance
+            Ypred[i] = self.ytr[min_index] # predict label of nearest example
+        return Ypred
+        
